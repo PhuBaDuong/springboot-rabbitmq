04b0c6ab287b4539b2aaea536778e6bd4c647921
Phu Ba Duong <bdp-ext@deveryware.net>
2023-08-19 00:09:39 +0700

    send string msg via rabbitmq

---
+++ src/main/java/techd/guide/springbootrabbitmq/controller/MessageController.java
@@ -0,0 +1,22 @@
+package techd.guide.springbootrabbitmq.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import techd.guide.springbootrabbitmq.publisher.RabbitMQProducer;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class MessageController {
+    private final RabbitMQProducer rabbitMQProducer;
+
+    @GetMapping("/publish")
+    public ResponseEntity<String> sendMessage(@RequestParam("message") String message) {
+        rabbitMQProducer.sendMessage(message);
+        return ResponseEntity.ok("Message sent to rabbitMQ...");
+    }
+}
